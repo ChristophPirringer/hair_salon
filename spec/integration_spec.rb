138fe433e1bfb04c1e344a4af46f146f73b90e22
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-21 11:14:03 -0700

    Add integration test for removing all stylists from teh database. pass that spec.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -14,5 +14,16 @@ describe('adding a new stylist', {:type => :feature}) do
 		click_button('Add Stylist!')
 		expect(page).to have_content('JeanJaque')
 	end
+end
 
+describe('removing all stylists', {:type => :feature}) do
+  it('allows a user to remove all stylists from the database') do
+    visit('/')
+    click_link('Add New Brilliant Stylist!')
+    fill_in('name', :with => 'JeanJaque')
+    click_button('Add Stylist!')
+    click_link('Fire the lot!')
+    click_link('View All Our Brilliant Stylists!')
+    expect(page).to have_no_content('JeanJaque')
+  end
 end
