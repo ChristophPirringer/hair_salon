49e0a336b8f0831fe1084af21a6e582aad1ab3a2
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-21 10:41:39 -0700

    add methods and specs to read out the stylist-id of a client and equal identical clients. Pass the specs.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -23,4 +23,21 @@ end
 			test_client = Client.new({:client_name => "Bob", :stylist_id => 1})
 			expect(test_client.client_name()).to(eq("Bob"))
 		end
+
+
+	describe("#stylist_id") do
+		it("lets you read the stylist ID out") do
+			test_client = Client.new({:client_name => "Bob", :stylist_id => 1})
+			expect(test_client.stylist_id()).to(eq(1))
+		end
 	end
+
+	describe("#==") do
+		it("is the same client if it has the same client_name") do
+			test_client1 = Client.new({:client_name => "Bob", :stylist_id => 1})
+			test_client2 = Client.new({:client_name => "Bob", :stylist_id => 1})
+			expect(test_client1).to(eq(test_client2))
+		end
+	end
+
+end
