992150a1d4fd47df4f5d8b57dca7b935fc0ab83d
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-21 10:37:27 -0700

    add client_name spec. pass the spec.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,3 +9,18 @@ describe(Client) do
 	end
 
 end
+
+	describe("#save") do
+		it("adds a client to the array of saved clients") do
+			test_client = Client.new({:client_name => "Bob", :stylist_id => 1})
+			test_client.save()
+			expect(Client.all()).to(eq([test_client]))
+		end
+	end
+
+	describe("#client_name") do
+		it("lets you give a client_name") do
+			test_client = Client.new({:client_name => "Bob", :stylist_id => 1})
+			expect(test_client.client_name()).to(eq("Bob"))
+		end
+	end
